ff7c7c4cc3b2eb442bc571ba13ba2950a913f7e6
dinud143 <35167931+dinud143@users.noreply.github.com>
2018-01-10 16:51:30 +0530

    Update enrollHeaderTest.py

--- enrollHeaderTest.py
+++ enrollHeaderTest.py
@@ -1,5 +1,5 @@
 # Copyright (c) 2017 Adafruit Industries
-# Author: Tony DiCola & James DeVito
+# Author: Dinu D
 #
 # Permission is hereby granted, free of charge, to any person obtaining a copy
 # of this software and associated documentation files (the "Software"), to deal
